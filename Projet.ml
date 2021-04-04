37ba08967e227b72a1efde2d96898f57800e24c7
Phaeon <46031169+Phaeon@users.noreply.github.com>
2021-04-04 14:55:59 +0200

    Add files via upload

--- Projet.ml
+++ Projet.ml
@@ -24,45 +24,64 @@ type expr_reg = Vide
 		==> Expressions régulières
 *)
 
-(* 	FONCTIONS UTILES
-
-  - List.mem : Vérifier l'appartenance d'un élément à une liste
-  - List.map : Appliquer une fonction à tous les éléments d'une liste
-  - List.fold_left / fold_right : 
-  - List.find : Retourne le premier élément qui satisfait un prédicat
-  - List.find_all : Retourne tous les éléments qui valident le prédicat
-  - List.exists : Vérifie si au moins un élément valide le prédicat p
-  - List.for_all : Teste un prédicat sur une liste
-
-
-*)
-
 
 (* Exemple du sujet *)
 let trans = [(0,[('a',0);('b',1)]);(1,[('b',1);('a',2)]);(2,[('a',2);('b',3)]);(3,[('a',0);('b',3)])] ;;
 
+(* Mon exemple *)
+let trans2 = [(0, [('a', 1);('b', 2);('c', 3)]);
+		(1, [('a', 1);('b', 0);('c', 2)]);
+		(2, [('a', 2);('b', 0);('c', 3)]);
+		(3, [('a', 3);('b', 0);('c', 0)])] ;;
+
+
+
 (* 1. Généraliser un AFD à partir d'un graphe *)
-let rec char_en_expr_reg c = match c with
-	[] -> []
-	| (c, et)::a -> (S(c), et)::(char_en_expr_reg a)
+
+(* Trier les transitions *)
+let rec trier nb_etats trans = 
+	if nb_etats > 0 then
+		if List.exists (fun (x,y) -> y = (nb_etats - 1)) trans then 
+			(List.find_all (fun (x,y) -> y = (nb_etats - 1)) trans)@(trier (nb_etats - 1) trans)
+		else (trier (nb_etats - 1) trans)
+	else []
 ;;
 
-char_en_expr_reg [('a',0)] ;;
+trier 4 (snd (List.nth trans2 3));;
 
-let rec generaliser_afd transitions = match transitions with
+let rec char_en_expr_reg_ p =
+	match p with
+		[] -> Vide
+		| (t, e)::r -> let l = List.length r in 
+			if l > 0 then Ou(S(t), char_en_expr_reg_ r) 
+			else S(t)
+;;
+
+(* Généralise les transitions d'un AFD *) 
+let rec char_en_expr_reg nb_etats p =
+	if nb_etats > 0 then
+		let pp = List.find_all (fun (x,y) -> y = (nb_etats - 1)) p in
+			if List.length pp = 1 then (S(fst (List.hd pp)), (nb_etats - 1))::(char_en_expr_reg (nb_etats - 1) p)
+			else if List.length pp > 1 then (char_en_expr_reg_ pp, (nb_etats - 1))::(char_en_expr_reg (nb_etats - 1) p)
+			else (char_en_expr_reg (nb_etats - 1) p)
+	else []
+;;
+
+
+let rec generaliser_afd nb_etats transitions = match transitions with
 	[] -> []
-	| (e, t)::r -> (e, char_en_expr_reg t)::(generaliser_afd r)
+	| (e, t)::r -> (e, char_en_expr_reg nb_etats (trier nb_etats t))::(generaliser_afd nb_etats r)
 ;;
 
 (* Test de la fonction generaliser_afd *)
-let trans_gene = generaliser_afd trans ;;
+let trans_gene = generaliser_afd 4 trans2 ;;
 
 
 
 
 
 
-(* 2. Retourner une chaîne de caractères à partir d'un graphe *)
+(* 2. Retourner une chaîne de caractères à partir d'un graphe *) (* OK *)
 let rec expr_en_chaine er = match er with
 	Vide -> ""
 	| Epsilon -> ""
@@ -84,8 +103,10 @@ let trans_test = expr_en_chaine expr_1 ;;
 
 (* Fonction annexe : Récupère les transitions de l'état eliminé *)
 let rec transitions_etat etat transitions =
-	let p = List.find (fun (x, y) -> etat = x) transitions in snd p ;;
-
+	if List.exists (fun (x, y) -> etat = x) transitions then
+		let p = List.find (fun (x, y) -> etat = x) transitions in snd p (* ICI *)
+	else []
+;;
 
 (* Fonction annexe : Transforme la fonction de transition de l'état supprimé vers lui-même *)
 let modif_trans_meme_etat etat transition = match transition with
@@ -118,8 +139,6 @@ let rec diff_etat_trans etat_elimine trans_extrait = match trans_extrait with
 	| (t, e)::r -> if e = etat_elimine then diff_etat_trans etat_elimine r else (t, e)::(diff_etat_trans etat_elimine r)
 ;;
 
-
-
 (* Paramètres : Etat courant, Etat supprimé, Tête tu couple gardé courant, Regex de l'état éliminé, Translations du noeud courant *)
 let rec transformer_etat_ etat etat_supp tt eli transitions = match transitions with
 	[] -> []
@@ -134,6 +153,12 @@ let rec transformer_etat etat_supp transitions eli t_gard = match t_gard with
 			p@(transformer_etat etat_supp transitions eli r)
 ;;
 
+(* Fonction annexe : Supprime les doublons *)
+let rec doublons l = match l with
+	[] -> []
+	| x::r -> if List.mem x r then doublons r
+		 else x::(doublons r) ;;
+
 (* Eliminer un état et renvoyer le graphe correspondant *)
 let rec eliminer_un_etat etat graphe = 
 		match graphe with
@@ -142,7 +167,7 @@ let rec eliminer_un_etat etat graphe =
 						if trouver_predecesseur etat t then 
 							let t_eli = fst (List.hd (trans_etat_supp etat (meme_etat_trans etat (transitions_etat etat graphe)))) in 
 								let t_gard = diff_etat_trans etat (transitions_etat etat graphe) in
-								 	(e, transformer_etat etat t t_eli t_gard)::(eliminer_un_etat etat r)
+								 	(e, doublons (transformer_etat etat t t_eli t_gard))::(eliminer_un_etat etat r)
 						else (e,t)::(eliminer_un_etat etat r)
 ;;
 
@@ -168,13 +193,22 @@ let trans_reduit = eliminer_tous [0;3] 4 trans_gene ;;
 (* Convertir un AFD réduit en expression régulière *)
 let expression_reguliere etat_initial etat_final graphe_reduit =
 	let tete = transitions_etat etat_initial graphe_reduit in
-		let r = fst (List.find (fun (x,y) -> y = etat_initial) tete) in
-			if (etat_initial = etat_final) then Etoile(r)
-			else let queue = transitions_etat etat_final graphe_reduit in
-				let s = fst (List.find (fun (x,y) -> not (y = etat_initial)) tete) in
-					let t = fst (List.find (fun (x,y) -> y = etat_final) queue) in
-						let u = fst (List.find (fun (x,y) -> not (y = etat_final)) queue) in
-				Concat(Concat(Etoile(r), s), Etoile(Ou(t,Concat(u, Concat(Etoile(r), s))))) ;;
+		if List.exists (fun (x,y) -> y = etat_initial) tete then
+			let r = fst (List.find (fun (x,y) -> y = etat_initial) tete) in (* ICI *)
+				if (etat_initial = etat_final) then Etoile(r)
+				else let queue = transitions_etat etat_final graphe_reduit in
+					if List.exists (fun (x,y) -> not (y = etat_initial)) tete then
+						let s = fst (List.find (fun (x,y) -> not (y = etat_initial)) tete) in (* ICI *)
+							if List.exists (fun (x,y) -> y = etat_final) queue then
+								let t = fst (List.find (fun (x,y) -> y = etat_final) queue) in (* ICI *) 
+									if List.exists (fun (x,y) -> not (y = etat_final)) queue then
+										let u = fst (List.find (fun (x,y) -> not (y = etat_final)) queue) in (* ICI *)
+						Concat(Concat(Etoile(r), s), Etoile(Ou(t,Concat(u, Concat(Etoile(r), s))))) 
+									else failwith "ERREUR"
+							else failwith "ERREUR : Aucun état final."
+					else failwith "ERREUR : Il manque des états."
+		else failwith "ERREUR : Aucun état initial."
+;;
 
 let er03 = expression_reguliere 0 3 trans_reduit ;;
 
@@ -192,7 +226,6 @@ let rec liste_afd_reduits nb_etats etat_initial etats_finaux transitions =
 ;;
 
 liste_afd_reduits 4 0 [3;2] trans_gene ;;
-(* DOnne : Couples (etat final, afd réduit) *)
 
 
 
@@ -204,10 +237,15 @@ let rec afd_en_expr_reg_ liste etat_initial =
 
 
 let afd_en_expr_reg nb_etats etat_initial etats_finaux transitions =
-	let afd_gen = generaliser_afd transitions in
-		let liste_afds = liste_afd_reduits nb_etats etat_initial etats_finaux afd_gen in afd_en_expr_reg_ liste_afds etat_initial
+	let afd_gen = generaliser_afd nb_etats transitions in
+		let liste_afds = liste_afd_reduits nb_etats etat_initial etats_finaux afd_gen in 
+			if (List.length liste_afds = 1) then expression_reguliere etat_initial (List.hd etats_finaux) (snd (List.hd liste_afds))
+			else afd_en_expr_reg_ liste_afds etat_initial
 ;;
 
 
 afd_en_expr_reg 4 0 [3] trans ;;
+afd_en_expr_reg 4 0 [3] trans2 ;;
+
+
 
